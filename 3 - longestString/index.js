77ed91f977d71d668adea679f28e56ce2c2b5acb
-.- <delciopolanco@gmail.com>
2021-07-23 13:30:59 -0400

    exercice 3 and 4

---
+++ 3 - longestString/index.js
@@ -0,0 +1,43 @@
+var verifyLargest = function (largest, strings) {
+  if (largest.length < strings.length) {
+    largest = strings;
+    strings = '';
+  }
+
+  return largest;
+};
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  var elements = [];
+  var strings = '';
+  var largest = '';
+  var increment = 0;
+
+  for (var x = 0; x <= s.length - 1; x++) {
+    if (!elements[increment] || elements[increment].indexOf(s[x]) < 0) {
+      strings += s[x];
+      elements[increment] = strings;
+
+      largest = verifyLargest(largest, strings);
+
+    } else {
+
+      largest = verifyLargest(largest, strings);
+
+      increment++;
+      elements[increment] = s[x];
+      strings = s[x];
+    }
+  }
+
+  return largest.length;
+};
+
+console.log(lengthOfLongestSubstring('abcabcbb'));
+console.log(lengthOfLongestSubstring('bbbbb'));
+console.log(lengthOfLongestSubstring('pwwkew'));
+console.log(lengthOfLongestSubstring(''));
